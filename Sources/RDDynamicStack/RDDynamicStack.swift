c9bb153b666958659ccfa19ddb5325b4079d73ba
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2024-09-19 22:46:16 +0900

    Initial Commit

--- Sources/RDDynamicStack/RDDynamicStack.swift
+++ Sources/RDDynamicStack/RDDynamicStack.swift
@@ -1,2 +1,38 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  RDDynamicStack.swift
+//  RDDynamicStack
+//
+//  https://github.com/RyoDeveloper/RDDynamicStack
+//  Copyright © 2024 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+public struct RDDynamicStack<Content: View>: View {
+    private let content: () -> Content
+    private let layout: AnyLayout
+
+    public init(
+        axis: Axis, spacing: CGFloat? = nil,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.content = content
+        self.layout =
+            axis == .horizontal
+            ? AnyLayout(HStackLayout(spacing: spacing))
+            : AnyLayout(VStackLayout(spacing: spacing))
+    }
+
+    public var body: some View {
+        layout {
+            content()
+        }
+    }
+}
+
+#Preview {
+    RDDynamicStack(axis: .vertical) {
+        Text("View1")
+        Text("View2")
+    }
+}
